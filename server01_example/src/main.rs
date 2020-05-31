6302e039753a8fd06ccd18a1de19bfb424e0c4b2
birkin <birkin.diana@gmail.com>
2020-05-31 07:36:49 -0400

    starts copy/grok.

--- server01_example/src/main.rs
+++ server01_example/src/main.rs
@@ -7,5 +7,20 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:6142";
+    let mut listener = TcpListener::bind( addr ).await.unwrap();
+
+    let server = {
+        async move {  // what's this `move`?
+            let mut incoming = listener.incoming();
+            while let Some( conn ) = incoming.next().await {
+                match conn {
+                    Err(e) => eprintln!( "accept failed = ``{:?}``", e ),  // whats this `eprintln!`?
+                    Ok( HEREZZ )
+                }
+
+            }
+        }
+    };
+
 }
